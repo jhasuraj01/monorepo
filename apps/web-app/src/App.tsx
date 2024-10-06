bff0f92f421ba0ff9a013894439155237f055a1d
Suraj Jha <44930179+jhasuraj01@users.noreply.github.com>
2024-10-06 11:38:53 +0000

    refactor: added types in App.tsx

--- apps/web-app/src/App.tsx
+++ apps/web-app/src/App.tsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App: React.FC = () => {
+  const [count, setCount] = useState<number>(0);
 
   return (
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+    <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
