22c385d343d99d4b9667eeeadfa35ab3f43f2916
Suraj Jha <44930179+jhasuraj01@users.noreply.github.com>
2025-04-12 14:15:53 +0000

    fix: templates env not getting updated

--- cmds/template-save.sh
+++ cmds/template-save.sh
@@ -8,42 +8,50 @@ template_names=(
   "package"
 )
 
-# Validate the input
-if [ -z "$1" ] || [ -z "$2" ]; then
-    echo "Usage: $0 <template_name> <source_path>"
-    exit 1
-fi
-
 # Main script
-script_path=$(realpath "$0") # Get the full path of this script
-template_name="$1" # Get the name of the template
-source_path=$(realpath "$2") # Get the full path of the target directory
+script_path=$(realpath "$0")
+template_name="${1:-all}"       # Default to all
+source_path_raw="${2:-./templates}"         # Default to current directory
+source_path=$(realpath "$source_path_raw")
 
 # Generate Paths
 script_dir=$(dirname "$script_path")
 output_zip_dir=$(realpath "$script_dir/$template_dir")
-output_zip="$output_zip_dir/$template_name.zip"
 
-if [ ! -d "$source_path" ]; then
-    echo "Target path does not exist or is not a directory."
-    exit 1
+# setup output directory
+rm -rf "$output_zip_dir"
+mkdir -p "$output_zip_dir"
+
+save_template() {
+    local name="$1"
+    local src="$source_path/$name"
+    local out_zip="$output_zip_dir/$name.zip"
+
+    if [ ! -d "$src" ]; then
+        echo "Error: save template failed due to '$name' not found at $src"
+        exit 1
+    fi
+
+    echo "=> Saving '$name' template from $src"
+    echo "-----------------------------------------------------------------------------------------------------"
+
+    cd "$src" || { echo "Error: Failed to enter $src"; exist 1; }
+    rm -f "$out_zip"
+    zip -r -q "$out_zip" . || { echo "Error: Failed to zip $src"; exit 1; }
+
+    echo "✅ Saved: $out_zip"
+    echo
+}
+
+if [ "$template_name" == "all" ]; then
+    for name in "${template_names[@]}"; do
+        save_template "$name"
+    done
+else
+    if [[ ! " ${template_names[@]} " =~ " ${template_name} " ]]; then
+        echo "Invalid template name. Available templates: ${template_names[*]}"
+        exit 1
+    fi
+
+    save_template "$template_name"
 fi
-
-if [[ ! ${template_names[@]} =~ ${template_name} ]]; then
-    echo "Invalid template name. Available templates: ${template_names[*]}"
-    exit 1
-fi
-
-if [ ! -d "$output_zip_dir" ]; then
-    mkdir -p "$output_zip_dir"
-fi
-
-echo "=> Creating $template_name template from source path: $source_path"
-echo "====================================================================================================="
-
-cd "$source_path" || exit 1 # Change to the source directory
-rm $output_zip -rf # Remove the existing zip archive
-zip -r "$output_zip" . || exit 1 # Create the zip archive
-
-echo "====================================================================================================="
-echo "=> Template saved successfully to $output_zip"
